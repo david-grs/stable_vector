8feb0949c988d29af9ea3b917b6f5cf6290a93d7
David Grs <david.a.grs@gmail.com>
2018-03-04 17:00:18 +0100

    refactoring

--- stable_vector.h
+++ stable_vector.h
@@ -95,63 +95,19 @@ public:
 	};
 
 	stable_vector() = default;
-
-	explicit stable_vector(size_type count, const T& value)
-	{
-		for (size_type i = 0; i < count; ++i)
-		{
-			push_back(value);
-		}
-	}
-
-	explicit stable_vector(size_type count)
-	{
-		for (size_type i = 0; i < count; ++i)
-		{
-			emplace_back();
-		}
-	}
+	explicit stable_vector(size_type count, const T& value);
+	explicit stable_vector(size_type count);
 
 	template <typename InputIt,
-			  typename =
-				  typename std::enable_if<
-					  std::is_convertible<
-						  typename std::iterator_traits<InputIt>::iterator_category, std::input_iterator_tag
-			  >::value>::type>
-	stable_vector(InputIt first, InputIt last)
-	{
-		for (; first != last; ++first)
-		{
-			push_back(*first);
-		}
-	}
+			  typename = std::enable_if_t<std::is_convertible<typename std::iterator_traits<InputIt>::iterator_category, std::input_iterator_tag>::value>>
+	stable_vector(InputIt first, InputIt last);
 
-	stable_vector(const stable_vector& other)
-	{
-		for (const auto& chunk : other.m_chunks)
-		{
-			m_chunks.emplace_back(std::make_unique<chunk_type>(*chunk));
-		}
-	}
+	stable_vector(std::initializer_list<T>);
 
-	stable_vector(stable_vector&& other) :
-		m_chunks(std::move(other.m_chunks))
-	{
-	}
+	stable_vector(const stable_vector& other);
+	stable_vector(stable_vector&& other);
 
-	stable_vector(std::initializer_list<T> ilist)
-	{
-		for (const auto& t : ilist)
-		{
-			push_back(t);
-		}
-	}
-
-	stable_vector& operator=(stable_vector v)
-	{
-		swap(v);
-		return *this;
-	}
+	stable_vector& operator=(stable_vector v);
 
 	iterator begin() { return {this, 0}; }
 	iterator end()   { return {this, size()}; }
@@ -162,21 +118,12 @@ public:
 	const_iterator cbegin() const { return begin(); }
 	const_iterator cend()   const { return end(); }
 
-	size_type size() const
-	{
-		return std::accumulate(m_chunks.cbegin(), m_chunks.cend(), size_type{}, [](size_type s, auto& chunk_ptr)
-							   {
-								   return s + chunk_ptr->size();
-							   });
-	}
+	size_type size() const;
 
 	size_type capacity() const { return m_chunks.size() * ChunkSize; }
 	bool empty() const { return m_chunks.empty(); }
 
-	bool operator==(const __self& c) const
-	{
-		return size() == c.size() && std::equal(cbegin(), cend(), c.cbegin());
-	}
+	bool operator==(const __self& c) const { return size() == c.size() && std::equal(cbegin(), cend(), c.cbegin()); }
 
 	bool operator!=(const __self& c) const { return !operator==(c); }
 
@@ -191,62 +138,180 @@ public:
 	const_reference back() const { return back(); }
 
 private:
-	void add_chunk() {  m_chunks.emplace_back(std::make_unique<chunk_type>()); }
-
-	chunk_type& current_chunk()
-	{
-		if (likely_false(m_chunks.empty() || m_chunks.back()->size() == ChunkSize))
-		{
-			add_chunk();
-		}
-
-		return *m_chunks.back();
-	}
+	void add_chunk();
+	chunk_type& current_chunk();
 
 public:
-	void reserve(size_type new_capacity)
-	{
-		const std::size_t initial_capacity = capacity();
-		for (difference_type i = new_capacity - initial_capacity; i > 0; i -= ChunkSize)
-		{
-			add_chunk();
-		}
-	}
+	void reserve(size_type new_capacity);
 
-	void push_back(const T& t) { current_chunk().push_back(t); }
-	void push_back(T&& t)      { current_chunk().push_back(std::move(t)); }
+	void push_back(const T& t);
+	void push_back(T&& t);
 
 	template <typename... Args>
-	void emplace_back(Args&&... args)
-	{
-		current_chunk().emplace_back(std::forward<Args>(args)...);
-	}
+	void emplace_back(Args&&... args);
 
-	reference operator[](size_type i)
-	{
-		return (*m_chunks[i / ChunkSize])[i % ChunkSize];
-	}
+	reference operator[](size_type i);
 
-	const_reference operator[](size_type i) const
-	{
-		return const_cast<__self&>(*this)[i];
-	}
+	const_reference operator[](size_type i) const;
 
-	reference at(size_type i)
-	{
-		if (likely_false(i >= size()))
-		{
-			throw std::out_of_range("stable_vector::at");
-		}
+	reference at(size_type i);
 
-		return operator[](i);
-	}
-
-	const_reference at(size_type i) const
-	{
-		return const_cast<__self&>(*this).at(i);
-	}
+	const_reference at(size_type i) const;
 
 private:
 	storage_type m_chunks;
 };
+
+
+
+
+
+
+
+template <typename T, std::size_t ChunkSize>
+stable_vector<T, ChunkSize>::stable_vector(size_type count, const T& value)
+{
+	for (size_type i = 0; i < count; ++i)
+	{
+		push_back(value);
+	}
+}
+
+template <typename T, std::size_t ChunkSize>
+stable_vector<T, ChunkSize>::stable_vector(size_type count)
+{
+	for (size_type i = 0; i < count; ++i)
+	{
+		emplace_back();
+	}
+}
+
+template <typename T, std::size_t ChunkSize>
+template <typename InputIt, typename>
+stable_vector<T, ChunkSize>::stable_vector(InputIt first, InputIt last)
+{
+	for (; first != last; ++first)
+	{
+		push_back(*first);
+	}
+}
+
+template <typename T, std::size_t ChunkSize>
+stable_vector<T, ChunkSize>::stable_vector(const stable_vector& other)
+{
+	for (const auto& chunk : other.m_chunks)
+	{
+		m_chunks.emplace_back(std::make_unique<chunk_type>(*chunk));
+	}
+}
+
+template <typename T, std::size_t ChunkSize>
+stable_vector<T, ChunkSize>::stable_vector(stable_vector&& other) :
+	m_chunks(std::move(other.m_chunks))
+{
+}
+
+template <typename T, std::size_t ChunkSize>
+stable_vector<T, ChunkSize>::stable_vector(std::initializer_list<T> ilist)
+{
+	for (const auto& t : ilist)
+	{
+		push_back(t);
+	}
+}
+
+template <typename T, std::size_t ChunkSize>
+stable_vector<T, ChunkSize>& stable_vector<T, ChunkSize>::operator=(stable_vector v)
+{
+	swap(v);
+	return *this;
+}
+
+template <typename T, std::size_t ChunkSize>
+typename stable_vector<T, ChunkSize>::size_type stable_vector<T, ChunkSize>::size() const
+{
+	return std::accumulate(m_chunks.cbegin(), m_chunks.cend(), size_type{}, [](size_type s, auto& chunk_ptr)
+						   {
+							   return s + chunk_ptr->size();
+						   });
+}
+
+template <typename T, std::size_t ChunkSize>
+void stable_vector<T, ChunkSize>::add_chunk()
+{
+	m_chunks.emplace_back(std::make_unique<chunk_type>());
+}
+
+template <typename T, std::size_t ChunkSize>
+typename stable_vector<T, ChunkSize>::chunk_type& stable_vector<T, ChunkSize>::current_chunk()
+{
+	if (likely_false(m_chunks.empty() || m_chunks.back()->size() == ChunkSize))
+	{
+		add_chunk();
+	}
+
+	return *m_chunks.back();
+}
+
+template <typename T, std::size_t ChunkSize>
+void stable_vector<T, ChunkSize>::reserve(size_type new_capacity)
+{
+	const std::size_t initial_capacity = capacity();
+	for (difference_type i = new_capacity - initial_capacity; i > 0; i -= ChunkSize)
+	{
+		add_chunk();
+	}
+}
+
+template <typename T, std::size_t ChunkSize>
+void stable_vector<T, ChunkSize>::push_back(const T& t)
+{
+	current_chunk().push_back(t);
+}
+
+template <typename T, std::size_t ChunkSize>
+void stable_vector<T, ChunkSize>::push_back(T&& t)
+{
+	current_chunk().push_back(std::move(t));
+}
+
+template <typename T, std::size_t ChunkSize>
+template <typename... Args>
+void stable_vector<T, ChunkSize>::emplace_back(Args&&... args)
+{
+	current_chunk().emplace_back(std::forward<Args>(args)...);
+}
+
+template <typename T, std::size_t ChunkSize>
+typename stable_vector<T, ChunkSize>::reference
+stable_vector<T, ChunkSize>::operator[](size_type i)
+{
+	return (*m_chunks[i / ChunkSize])[i % ChunkSize];
+}
+
+template <typename T, std::size_t ChunkSize>
+typename stable_vector<T, ChunkSize>::const_reference
+stable_vector<T, ChunkSize>::operator[](size_type i) const
+{
+	return const_cast<__self&>(*this)[i];
+}
+
+template <typename T, std::size_t ChunkSize>
+typename stable_vector<T, ChunkSize>::reference
+stable_vector<T, ChunkSize>::at(size_type i)
+{
+	if (likely_false(i >= size()))
+	{
+		throw std::out_of_range("stable_vector::at");
+	}
+
+	return operator[](i);
+}
+
+template <typename T, std::size_t ChunkSize>
+typename stable_vector<T, ChunkSize>::const_reference
+stable_vector<T, ChunkSize>::at(size_type i) const
+{
+	return const_cast<__self&>(*this).at(i);
+}
+
